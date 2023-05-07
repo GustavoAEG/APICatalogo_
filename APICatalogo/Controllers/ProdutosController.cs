491d7e8d5b91a4fac5393b1c29267d7c1261f702
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2023-05-07 02:32:12 -0300

    Add project files.

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,24 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+
+
+    [Route("[controller]")]
+    [ApiController]
+
+    public class ProdutosController : ControllerBase
+    {
+
+        //1° passo injetar uma instancia do appdbContext na pasta context no CONSTRUTOR
+
+        private readonly AppDbContext _context;
+
+        //Gerado o CONSTRUTOR Da instancia da classe appdbcontext.
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
